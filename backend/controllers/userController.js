af7649cc4c645f1ab51943d02f4f651217b6f625
Marwan A <marwane_akiki@live.com>
2025-05-07 15:59:01 +0200

    Added users, signup

---
+++ backend/controllers/userController.js
@@ -0,0 +1,99 @@
+const User = require('../models/userModel')
+// const mongoose = require('mongoose');
+
+// // GET all
+// const getPackages = async (req, res) => {
+//     const packages = await Package.find({}).sort({createdAt: -1})
+//     res.status(200).json(packages)
+// }
+
+// // GET single
+// const getPackage = async (req, res) => {
+//     const { id } = req.params
+
+//     if (!mongoose.Types.ObjectId.isValid(id)) {
+//         return res.status(404).json({ error: 'No such package' });
+//     }
+
+//     const package = await Package.findById(id)
+
+//     if (!package) {
+//         return res.status(404).json({error: 'No such package'})
+//     }
+//     res.status(200).json(package)
+// }
+
+// POST single
+const loginUser = async (req, res) => {
+
+    try{
+        // const package = await Package.create({content, owner, weight})
+        res.json({mssg: 'login'})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
+}
+
+// POST single
+const signupUser = async (req, res) => {
+    const { email, password} = req.body
+
+    try{
+        const user = await User.signup(email, password)
+        res.status(200).json({email, user})
+    } catch (error) {
+        res.status(400).json({error: error.message})
+    }
+
+}
+
+// // DELETE single
+// const deletePackage = async (req, res) => {
+//     const { id } = req.params;
+  
+//     if (!mongoose.Types.ObjectId.isValid(id)) {
+//       return res.status(400).json({ error: 'No such Package' });
+//     }
+  
+//     const package = await Package.findById(id);
+  
+//     if (!package) {
+//       return res.status(400).json({ error: 'No such package' });
+//     }
+  
+//     // if (blog.authorid.toString() !== req.user._id.toString()) {
+//     //   return res.status(403).json({ error: 'You are not authorized to delete this blog' });
+//     // }
+  
+//     await Package.findByIdAndDelete(id);
+  
+//     // await User.findByIdAndUpdate(req.user._id, { $pull: { posts: blog._id } });
+  
+//     res.status(200).json(package);
+//   };
+
+// // PATCH single
+// const updatePackage = async (req, res) => {
+//     const { id } = req.params;
+  
+//     if (!mongoose.Types.ObjectId.isValid(id)) {
+//       return res.status(400).json({ error: 'No such package' });
+//     }
+  
+//     const package = await Package.findOneAndUpdate({ _id: id }, { ...req.body }, { new: true });
+  
+//     if (!package) {
+//       return res.status(400).json({ error: 'No such package' });
+//     }
+  
+//     res.status(200).json(package);
+// };
+
+module.exports = {
+    loginUser,
+    signupUser,
+//     getPackage,
+//     deletePackage,
+//     updatePackage
+}
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const packageRoutes = require('./routes/packages')
+const userRoutes = require('./routes/user')
 
 // Express APP
 const app = express()
@@ -16,6 +17,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.use('/api/packages', packageRoutes)
+app.use('/api/user', userRoutes)
 
 mongoose.connect(process.env.MONG_URI)
     .then(() => {
